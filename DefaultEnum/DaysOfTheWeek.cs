14b1e8fbb2f99cf58ee57025ff446a4980d1df83
David Gallivan <33528347+CodingWithDavid@users.noreply.github.com>
2019-04-01 08:27:15 -0700

    Initial Commit
    This is the initial commit of the solution for this project

---
+++ DefaultEnum/DaysOfTheWeek.cs
@@ -0,0 +1,75 @@
+﻿
+using System.ComponentModel;
+
+namespace DefaultEnum
+{
+    public enum DaysOfTheWeekSetByZeroValue
+    {
+        Monday = 0,
+        Tuesday = 1,
+        Wendesday = 2,
+        Thursday = 3,
+        Friday = 4,
+        Saturday = 5,
+        Sunday = 6,
+    }
+
+    [DefaultValue(Tuesday)]
+    public enum DOWSetByZeroValueWithDefaultValueAttribute
+    {
+        Monday = 0,
+        Tuesday = 1,
+        Wendesday = 2,
+        Thursday = 3,
+        Friday = 4,
+        Saturday = 5,
+        Sunday = 6,
+    }
+
+    [DefaultValue(Tuesday)]
+    public enum DefinedDOWSSetByDefaultValueAttribute
+    {
+        Monday,
+        Tuesday,
+        Wendesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday,
+    }
+
+    public enum NextDOWSetByZeroValueWithANegitive
+    {
+        NoDay = -1,
+        Tuesday = 1,
+        Wendesday = 2,
+        Thursday = 3,
+        Friday = 4,
+        Saturday = 5,
+        Sunday = 6,
+        Monday = 0,
+    }
+
+    public enum ByOrderDOWS
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wendesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+
+    public enum MultipleZeroDOW
+    {
+        Monday = 0,
+        NoDay = 0,
+        Tuesday = 1,
+        Wendesday = 2,
+        Thursday = 3,
+        Friday = 4,
+        Saturday = 5,
+        Sunday = 6
+    }
+}
---
+++ DefaultEnum/Program.cs
@@ -0,0 +1,99 @@
+﻿
+using System;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace DefaultEnum
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            WriteResults("Default string is", default(string));
+            WriteResults("Default int is", default(int).ToString());
+            WriteResults("Default sbyte is", default(sbyte).ToString());
+            WriteResults("Default byte is", default(byte).ToString());
+            WriteResults("Default uint is", default(uint).ToString());
+            WriteResults("Default char is", default(char).ToString());
+            WriteResults("Default bool is", default(bool).ToString());
+            WriteResults("Default long is", default(long).ToString());
+            WriteResults("Default double is", default(double).ToString());
+            WriteResults("Default float is", default(float).ToString());
+            WriteResults("Default decimal is", default(decimal).ToString());
+            WriteResults("Default point is", default(Point).ToString());
+            WritePersonResults("Default Person is");
+            WriteEnumResults("Default Enum is");
+
+            Console.WriteLine("");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("");
+
+            WriteResults("Default Enum set by zero value is", default(DaysOfTheWeekSetByZeroValue).ToString());
+            WriteResults("Default Enum set by zero value even with attribute set is", default(DOWSetByZeroValueWithDefaultValueAttribute).ToString());
+            WriteResults("Default Enum set by default value attribute is", default(DefinedDOWSSetByDefaultValueAttribute).ToString());
+
+            DefinedDOWSSetByDefaultValueAttribute o = GetDefaultValue<DefinedDOWSSetByDefaultValueAttribute>();
+            WriteResults("Use attribute for value is", o.ToString());
+
+            WriteResults("Default Enum set by zero value with a negitve value is", default(NextDOWSetByZeroValueWithANegitive).ToString());
+            WriteResults("Default Enum set by order of items in enum is", default(ByOrderDOWS).ToString());
+            WriteResults("Default Enum set by zero value with multiple zero values is", default(MultipleZeroDOW).ToString());
+
+            Console.ReadLine();
+        }
+
+        private static void WriteEnumResults(string message)
+        {
+            string result = "null";
+            Console.Write($"{message} ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Enum p = default(Enum);
+            if (p != null)
+            {
+                result = p.ToString();
+            }
+            Console.WriteLine(result);
+            Console.ResetColor();
+        }
+
+        private static void WritePersonResults(string message)
+        {
+            string result = "null";
+            Console.Write($"{message} ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Person p = default(Person);
+            if (p != null)
+            {
+                result = p.ToString();
+            }
+            Console.WriteLine(result);
+            Console.ResetColor();
+        }
+
+        private static void WriteResults(string message, string result)
+        {
+            Console.Write($"{message} ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            if(result == null)
+            {
+                result = "null";
+            }
+            Console.WriteLine(result);
+            Console.ResetColor();
+        }
+
+        private static TEnum GetDefaultValue<TEnum>() where TEnum : struct
+        {
+            Type t = typeof(TEnum);
+            DefaultValueAttribute[] attributes = (DefaultValueAttribute[])t.GetCustomAttributes(typeof(DefaultValueAttribute), false);
+            if (attributes?.Length > 0)
+            {
+                return (TEnum)attributes[0].Value;
+            }
+            else
+            {
+                return default(TEnum);
+            }
+        }
+    }
+}
